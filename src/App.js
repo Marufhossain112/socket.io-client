ec1a2aa4b45d086e6e39fa9e8980f08e08e78332
maruf <dev.maruf112@gmail.com>
2023-04-14 02:58:40 +0600

    send data from client to server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { io } from 'socket.io-client';
 function App() {
-  const socket = io("http://localhost:5000");
-  console.log(socket);
+  const socket = io.connect("http://localhost:5000");
+  // console.log(socket);
+  const [messege, setMessage] = useState("");
+  const handleSend = () => {
+    // console.log(messege);
+    socket.emit("sendMessageDataEvent", messege);
+  };
   return (
-    <div className="App">
+    <div style={{ textAlign: "center" }}>
       <h1>Hello bro....</h1>
+      <input onBlur={(e) => setMessage(e.target.value)} type="text" placeholder='messege...' />
+      <button onClick={handleSend()}>Send</button>
     </div>
   );
 }
