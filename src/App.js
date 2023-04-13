6341042224762e45bf7acbe5cb4f98992701b67a
maruf <dev.maruf112@gmail.com>
2023-04-14 03:37:51 +0600

    send and recieve message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 function App() {
   const socket = io.connect("http://localhost:5000");
   // console.log(socket);
-  const [messege, setMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const [getMessage, setGetMessage] = useState("");
   const handleSend = () => {
-    // console.log(messege);
-    socket.emit("sendMessageDataEvent", messege);
+    socket.emit("sendMessageDataEvent", message);
   };
+  useEffect(() => {
+    socket.on("showGetMessage", (data) => {
+      setGetMessage(data);
+    });
+  }, [socket]);
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Hello bro....</h1>
-      <input onBlur={(e) => setMessage(e.target.value)} type="text" placeholder='messege...' />
-      <button onClick={handleSend()}>Send</button>
+      <div className="container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <h2 style={{ color: "green" }}>Sender</h2> :  <h3 style={{ marginRight: "10px" }}>{message}</h3>
+        <h2 style={{ color: "red" }}>Receiver</h2> : <h3>{getMessage}</h3>
+      </div>
+      <input onChange={(e) => setMessage(e.target.value)} type="text" placeholder='message...' />
+      <button onClick={() => handleSend()}>Send</button>
     </div>
   );
 }
