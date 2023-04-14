fa1115b2afe2176e31849b9da85fd318dcc210ea
maruf <dev.maruf112@gmail.com>
2023-04-14 10:53:57 +0600

    room chat successful

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 function App() {
   const socket = io.connect("http://localhost:5000");
-  // console.log(socket);
   const [message, setMessage] = useState("");
   const [getMessage, setGetMessage] = useState("");
+  const [room, setRoom] = useState("");
   const handleSend = () => {
-    socket.emit("sendMessageDataEvent", message);
+    socket.emit("sendMessageDataEvent", { message, room });
+  };
+  const handleRoom = () => {
+    socket.emit("joinRoom", room);
   };
   useEffect(() => {
     socket.on("showGetMessage", (data) => {
-      setGetMessage(data);
+      setGetMessage(data.message);
+      // console.log(data);
     });
   }, [socket]);
   return (
@@ -18,7 +22,13 @@ function App() {
       <h1>Hello bro....</h1>
       <div className="container" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <h2 style={{ color: "green" }}>Sender</h2> :  <h3 style={{ marginRight: "10px" }}>{message}</h3>
-        <h2 style={{ color: "red" }}>Receiver</h2> : <h3>{getMessage}</h3>
+        <div>
+          <h2 style={{ color: "red" }}>Receiver</h2> : <h3>{getMessage}</h3>
+          <div>
+            <input onChange={(e) => setRoom(e.target.value)} type="text" placeholder='Join Room...' />
+            <button onClick={() => handleRoom()}>Join Room</button>
+          </div>
+        </div>
       </div>
       <input onChange={(e) => setMessage(e.target.value)} type="text" placeholder='message...' />
       <button onClick={() => handleSend()}>Send</button>
